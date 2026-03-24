33a487113ce74455bf02c3c85e2bbfdd6db8736d
holo-spice <2927970764@qq.com>
2026-03-24 07:32:12 -0700

    1

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -10,77 +10,91 @@
 uint64
 sys_exit(void)
 {
-  int n;
-  if(argint(0, &n) < 0)
-    return -1;
-  exit(n);
-  return 0;  // not reached
+	int n;
+	if (argint(0, &n) < 0)
+		return -1;
+	exit(n);
+	return 0; // not reached
 }
 
 uint64
 sys_getpid(void)
 {
-  return myproc()->pid;
+	return myproc()->pid;
 }
 
 uint64
 sys_fork(void)
 {
-  return fork();
+	return fork();
 }
 
 uint64
 sys_wait(void)
 {
-  uint64 p;
-  if(argaddr(0, &p) < 0)
-    return -1;
-  return wait(p);
+	uint64 p;
+	if (argaddr(0, &p) < 0)
+		return -1;
+	return wait(p);
 }
 
 uint64
 sys_sbrk(void)
 {
-  int addr;
-  int n;
+	int addr;
+	int n;
 
-  if(argint(0, &n) < 0)
-    return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
+	if (argint(0, &n) < 0)
+		return -1;
+	addr = myproc()->sz;
+
+	struct proc* p = myproc();
+	// n > 0：只增加进程大小（延迟分配场景下这里不立即分配物理页）
+	if(n > 0){
+		p->sz += n;
+	// n <= 0 且缩减后仍大于 0：回收对应虚拟地址区间
+	}else if(p->sz + n > 0){
+		p->sz = uvmdealloc(p->pagetable, p->sz, p->sz + n);
+	}else{
+		// 缩减到 0 或以下视为非法请求
+		return -1;
+	}
+	// if(growproc(n) < 0)
+	//   return -1;
+	return addr;
 }
 
 uint64
 sys_sleep(void)
 {
-  int n;
-  uint ticks0;
+	int n;
+	uint ticks0;
 
-  if(argint(0, &n) < 0)
-    return -1;
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
-      release(&tickslock);
-      return -1;
-    }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+	if (argint(0, &n) < 0)
+		return -1;
+	acquire(&tickslock);
+	ticks0 = ticks;
+	while (ticks - ticks0 < n)
+	{
+		if (myproc()->killed)
+		{
+			release(&tickslock);
+			return -1;
+		}
+		sleep(&ticks, &tickslock);
+	}
+	release(&tickslock);
+	return 0;
 }
 
 uint64
 sys_kill(void)
 {
-  int pid;
+	int pid;
 
-  if(argint(0, &pid) < 0)
-    return -1;
-  return kill(pid);
+	if (argint(0, &pid) < 0)
+		return -1;
+	return kill(pid);
 }
 
 // return how many clock tick interrupts have occurred
@@ -88,10 +102,10 @@ sys_kill(void)
 uint64
 sys_uptime(void)
 {
-  uint xticks;
+	uint xticks;
 
-  acquire(&tickslock);
-  xticks = ticks;
-  release(&tickslock);
-  return xticks;
+	acquire(&tickslock);
+	xticks = ticks;
+	release(&tickslock);
+	return xticks;
 }
